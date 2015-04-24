665a3393eba4264b1ff2b4ccfbab23d233e6c119
Martin Zuber <zubidlo@gmail.com>
2015-04-24 13:29:43 +0100

    assignment marked

--- 2_run_ChatClient.bat
+++ 2_run_ChatClient.bat
@@ -1,3 +1,3 @@
 cls
 SET CLASSES="./src;"
-java -Djava.security.manager -Djava.security.policy=chat.policy -cp %CLASSES% assignment_2.ChatClient 192.168.192.17 1099 ChatServer
+java -Djava.security.manager -Djava.security.policy=chat.policy -cp %CLASSES% assignment_2.ChatClient 10.7.65.98 1099 ChatServer
