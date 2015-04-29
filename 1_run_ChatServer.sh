6a3d0d48ccfe1305a651e8a8ab9a23300f3ca22c
Martin Zuber <zubidlo@gmail.com>
2015-04-29 19:49:51 +0100

    *.sh edited

--- 1_run_ChatServer.sh
+++ 1_run_ChatServer.sh
@@ -1,3 +1,3 @@
 clear
 CLASSES="src"
-java -Djava.security.manager -Djava.security.policy=chat.policy -cp $CLASSES assignment_2.ChatServer 1099 ChatServer
+java -Djava.security.manager -Djava.security.policy=chat.policy -cp $CLASSES assignment_2.ChatServer 1099
--- 2_run_ChatClient.sh
+++ 2_run_ChatClient.sh
@@ -1,3 +1,3 @@
 clear
 CLASSES="src"
-java -Djava.security.manager -Djava.security.policy=chat.policy -cp $CLASSES assignment_2.ChatClient 192.168.1.2 1099 ChatServer
+java -Djava.security.manager -Djava.security.policy=chat.policy -cp $CLASSES assignment_2.ChatClient rmi://192.168.192.17:1099/ChatServer
